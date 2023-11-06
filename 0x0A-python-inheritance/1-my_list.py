c4a0a7b3dcd362aeb6c3b0cbdbf684e5c47f53af
CyberGA <etomu.joshua@gmail.com>
2023-11-06 15:43:51 -0800

    class with method to sort list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 class MyList(list):
     """a derived class of list"""
 
+    def __init__(self):
+        """MyList constructor method"""
+        super()__init__()
+
     def print_sorted(self):
         """prints list in a sorted ascending order"""
         print(sorted(self))
