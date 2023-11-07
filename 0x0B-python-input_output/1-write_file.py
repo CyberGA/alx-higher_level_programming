c2495c5484019e9fac6e9a113e071776c22dac30
CyberGA <etomu.joshua@gmail.com>
2023-11-07 09:12:32 -0800

    writes content to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a function that writes to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        writes to a file
+
+        Args:
+        filename (file descriptor): file to be read
+        text (string): content to be written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
