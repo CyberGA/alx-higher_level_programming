f1488d8bffd2c7bb857dfaf467675728493f5a3c
CyberGA <etomu.joshua@gmail.com>
2023-11-07 09:44:35 -0800

    serialize an object and save to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines a function stores the string 
+representation to a text file
+"""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    saves an object string representation
+    to a file
+
+    Args:
+        my_obj (Object): Object to be saved
+        filename (str): file location
+    """
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
