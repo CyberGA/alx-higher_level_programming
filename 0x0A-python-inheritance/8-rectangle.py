648b405ab2b1658632200db132e4d219169635e9
CyberGA <etomu.joshua@gmail.com>
2023-11-06 16:46:33 -0800

    rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines the Rectangel subclass
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """constructor of rectangle class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
