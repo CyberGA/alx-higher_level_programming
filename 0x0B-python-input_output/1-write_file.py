c92eae4bbe7d8246e645ebffa3522b42bddac565
CyberGA <etomu.joshua@gmail.com>
2023-11-07 09:18:12 -0800

    writes content to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,11 +6,11 @@ Defines a function that writes to a file
 
 def write_file(filename="", text=""):
     """
-        writes to a file
+    writes to a file
 
-        Args:
-        filename (file descriptor): file to be read
-        text (string): content to be written
+    Args:
+        filename (str): file to be read
+        text (str): content to be written
     """
     with open(filename, "w", encoding="utf-8") as f:
-       return f.write(text)
+        return f.write(text)
