7d8b06cca1992ada4902606aa0ec875beff6f039
CyberGA <etomu.joshua@gmail.com>
2023-11-07 10:13:17 -0800

    deserialize data read from a file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyton3
+"""
+Defines a function that deserialize a
+string rep of an object read from a file
+"""
+
+
+def load_from_json_file(filename):
+    """
+    deserialize object data read from a file
+
+    Args:
+        filename (str): file path
+    """
+    with open(filename) as f:
+        return json.load(f)
