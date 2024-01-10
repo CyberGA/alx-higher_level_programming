1672bb52677475e38f7916f97810acbe90b7d0c9
CyberGA <etomu.joshua@gmail.com>
2024-01-09 17:12:28 -0800

    reversing a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   const listReversed = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    listReversed.push(i);
+    listReversed.push(list[i]);
   }
   return listReversed;
 };
