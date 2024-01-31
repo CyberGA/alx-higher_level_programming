1c5795c9781ddf9104e7e0a8f19027cd3a700b3e
CyberGA <etomu.joshua@gmail.com>
2024-01-30 16:32:27 -0800

    get request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-+ Python script that fetches https://alx-intranet.hbtn.io/status
-+ You must use the package requests
-+ You are not allow to import packages other than requests
-"""
+"""fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
