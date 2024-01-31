bb0e40160ea332bf8b2308e73d46b88c214d4a06
CyberGA <etomu.joshua@gmail.com>
2024-01-30 16:27:43 -0800

    fix: error handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
 
     try:
         with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
     except error.HTTPError as er:
         print('Error code:', er.code)
-    else:
-        print(res.read().decode('UTF-8'))
