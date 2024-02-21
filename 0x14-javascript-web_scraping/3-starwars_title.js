d2a2d04e83d16e26468bcdaa7fdcaead53f43d5f
CyberGA <etomu.joshua@gmail.com>
2024-02-20 20:50:37 -0800

    udpate

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const { argv } = process;
 const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
-request(url, { json: true }, (err, response, body) => {
+request.get(url, { json: true }, (err, response, body) => {
   if (err) console.log(err);
   else if (response && response.statusCode === 200) console.log(body.title);
 });
